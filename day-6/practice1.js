2ccdb98aed12e2aeee962ffd5a5b5c13273e03df
shivakumarmusani <shivakumarmusani@gmail.com>
2025-01-21 16:49:49 +0530

    added

---
+++ day-6/practice1.js
@@ -0,0 +1,9 @@
+let yourName = "shivakumar";
+let yourAge = 22;
+let isStudent = true;
+const graduationYear = 2023
+graduationYear = 2024;
+console.log(yourName);
+// console.log("Age:", yourAge);
+// console.log("Student Status:", isStudent);
+// console.log("Graduation Year:", graduationYear);
